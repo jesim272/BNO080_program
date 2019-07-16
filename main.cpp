0662fa2fe911826972ccf93694d0d68a4bf9f7f2
Jesi Miranda-Santos <45446341+jesim272@users.noreply.github.com>
2019-07-16 11:15:01 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2018 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "BNO080.h"
+DigitalOut led1(LED1);
+
+#define SLEEP_TIME                  500 // (msec)
+
+BNO080 imu;
+// main() runs in its own thread in the OS
+int main()
+{
+    int count = 0;
+    while (true) {
+        // Blink LED and wait 0.5 seconds
+        led1 = !led1;
+        wait_ms(SLEEP_TIME);
+        ++count;
+    }
+}
