874feeb38193b5c87d7384d80a76fbbfcfc1d959
Jesi Miranda-Santos <45446341+jesim272@users.noreply.github.com>
2019-07-18 15:13:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,48 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2018 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-<<<<<<< HEAD
-//fjamdkfjadklfjas;ldkfjadf
-//flakdja;dkjfa;kdfjsldfjkalkdfjlsdfkjskdjf
-=======
-#define another_mergge_conflict_for_johnny
->>>>>>> ae028f48479cacdaf5429bd82e3c9ef3232a6b3b
-#define merge_error_for_johnny
-#include "mbed.h"
-#include "BNO080.h"
-DigitalOut led1(LED1);
-
-#define SLEEP_TIME                  500 // (msec)
-
-
-BNO080::BNO080(Serial *debugPort, PinName user_SDApin, PinName user_SCLpin, PinName user_INTPin, PinName user_RSTPin,
-			   uint8_t i2cAddress, int i2cPortSpeed) :
-// main() runs in its own thread in the OS
+#include <mbed.h>
+#include <BNO080.h>
+ 
 int main()
 {
-    int count = 0;
-    while (true) {
-        // Blink LED and wait 0.5 seconds
-        led1 = !led1;
-        wait_ms(SLEEP_TIME);
-        ++count;
+    Serial pc(USBTX, USBRX);
+ 
+    // Create IMU, passing in output stream, pins, I2C address, and I2C frequency
+    // These pin assignments are specific to my dev setup -- you'll need to change them
+    BNO080 imu(&pc, D0, D1, D3, D4, 0x4a, 100000); 
+ 
+    pc.baud(9600);
+    pc.printf("============================================================\n");
+ 
+    // Tell the IMU to report rotation every 100ms and acceleration every 200ms
+    imu.enableReport(BNO080::ROTATION, 100);
+    imu.enableReport(BNO080::TOTAL_ACCELERATION, 200);
+ 
+    while (true)
+    {
+        wait(.001f);
+        
+        // poll the IMU for new data -- this returns true if any packets were received
+        if(imu.updateData())
+        {
+            // now check for the specific type of data that was received (can be multiple at once)
+            if (imu.hasNewData(BNO080::ROTATION))
+            {
+                // convert quaternion to Euler degrees and print
+                pc.printf("IMU Rotation Euler: ");
+                TVector3 eulerRadians = imu.rotationVector.euler();
+                TVector3 eulerDegrees = eulerRadians * (180.0 / M_PI);
+                eulerDegrees.print(pc, true);
+                pc.printf("\n");
+            }
+            if (imu.hasNewData(BNO080::TOTAL_ACCELERATION))
+            {
+                // print the acceleration vector using its builtin print() method
+                pc.printf("IMU Total Acceleration: ");
+                imu.totalAcceleration.print(pc, true);
+                pc.printf("\n");
+            }
+        }
     }
+ 
 }
-
+ 
+ 
