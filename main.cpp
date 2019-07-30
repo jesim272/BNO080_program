cb8568fdd4e7099bc21b384ce76c5953f8274331
Jesi Miranda-Santos <45446341+jesim272@users.noreply.github.com>
2019-07-30 10:05:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,47 +3,37 @@
 
 int main()
 {
-    Serial pc(USBTX, USBRX, 115200);
+    Serial pc(USBTX, USBRX, 57600);
+    
 
     // Create IMU, passing in output stream, pins, I2C address, and I2C frequency
     // These pin assignments are specific to my dev setup -- you'll need to change them
-    //pc.printf("============================================================\n");
+    BNO080 imu(&pc, D4, D5, D12, D10, 0x4b, 100000);
 
-    BNO080 imu(&pc, PB_7, PB_6, D12, PA_11, 0x4b, 400000);
-
-    //pc.printf("====================================  Entering imu.begin ===================================\r\n");
     imu.begin();
-    //pc.printf("======================================EXIT & ENABLING REPORT FOR IMU==========================================================\r\n");
+
     // Tell the IMU to report rotation every 100ms and acceleration every 200ms
+    imu.enableReport(BNO080::TOTAL_ACCELERATION, 100);
     imu.enableReport(BNO080::ROTATION, 100);
-    imu.enableReport(BNO080::TOTAL_ACCELERATION, 200);
-    wait(.002f);
-    //pc.printf("======================================DONE ENABLING REPORT FOR IMU ===================================================\r\n");
-    //pc.printf("=========================    STUCK INSIDE THE WHILE LOOP ===============================\r\n");
 
     while (true) {
         wait(.001f);
+
         // poll the IMU for new data -- this returns true if any packets were received
+
         if(imu.updateData()) {
-            //pc.printf("======================================= INSIDE IMU.UPDATEDATA() =================================\r\n");
             // now check for the specific type of data that was received (can be multiple at once)
-            if (imu.hasNewData(BNO080::ROTATION)) {
-                // convert quaternion to Euler degrees and print
-                pc.printf("IMU Rotation Euler: ");
+            if (imu.hasNewData(BNO080::TOTAL_ACCELERATION) || imu.hasNewData(BNO080::ROTATION)) {
+                pc.printf("Total Accel: ");
+                imu.totalAcceleration.print(pc, true);
+                
+                pc.printf(", Rotation:");
                 TVector3 eulerRadians = imu.rotationVector.euler();
                 TVector3 eulerDegrees = eulerRadians * (180.0 / M_PI);
                 eulerDegrees.print(pc, true);
                 pc.printf("\n");
             }
-            /*
-            if (imu.hasNewData(BNO080::TOTAL_ACCELERATION)) {
-                // print the acceleration vector using its builtin print() method
-                pc.printf("IMU Total Acceleration: ");
-                imu.totalAcceleration.print(pc, true);
-                pc.printf("\n");
-            }
-            */
-
+            
         }
     }
 
